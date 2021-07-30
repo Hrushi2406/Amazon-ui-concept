6c0ac26a14e303db6f62c726cbcd3ab624b31591
hrushikesh <kukularehrushi@gmail.com>
2021-07-30 14:41:48 +0530

    Completed menu animation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+    this.showMenu = true,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final bool showMenu;
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          IconButton(
+            onPressed: onPressed,
+            icon: Padding(
+              padding: EdgeInsets.only(top: showMenu ? 20.0 : 0),
+              child: Icon(
+                showMenu ? Icons.menu : Icons.close,
+                color: Colors.white,
+                size: showMenu ? 30 : 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:amazonui/screens/home_screen.dart';
 import 'package:amazonui/screens/nav_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,14 +11,105 @@ class Wrapper extends StatefulWidget {
   _WrapperState createState() => _WrapperState();
 }
 
-class _WrapperState extends State<Wrapper> {
+class _WrapperState extends State<Wrapper> with SingleTickerProviderStateMixin {
+  //Animation Controller
+  late final AnimationController _animationController;
+
+  //Scale animation for the home screen
+  late final Animation<double> _scaleAnimation;
+
+  //Transform animation for the home screen
+  late final Animation<Offset> _translateAnimation;
+
+  //Rotation animation for the home screen
+  late final Animation<double> _rotateAnimation;
+
+  final Curve curve = Curves.easeInOut;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 850),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1,
+      end: 0.85,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: curve,
+      ),
+    );
+
+    _translateAnimation = Tween<Offset>(
+      begin: Offset(0, 0),
+      end: Offset(0.65, 0.05),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: curve,
+      ),
+    );
+
+    _rotateAnimation = Tween<double>(
+      begin: 0,
+      end: -0.015,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: curve,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void handleMenu() {
+    if (showMenu) {
+      _animationController.forward();
+      showMenu = false;
+    } else {
+      _animationController.reverse();
+      showMenu = true;
+    }
+
+    setState(() {});
+  }
+
+  bool showMenu = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const [
-          NavScreen(),
-          HomeScreen(),
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: Colors.white60,
+              child: NavScreen(),
+            ),
+          ),
+          ScaleTransition(
+            scale: _scaleAnimation,
+            child: SlideTransition(
+              position: _translateAnimation,
+              child: RotationTransition(
+                alignment: Alignment.topCenter,
+                turns: _rotateAnimation,
+                child: HomeScreen(
+                  showMenu: showMenu,
+                  onPressed: handleMenu,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
