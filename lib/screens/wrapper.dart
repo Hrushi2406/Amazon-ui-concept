cdeb5d894cbd9906ad1834bfbcbfaee8d0c83392
hrushikesh <kukularehrushi@gmail.com>
2021-07-30 09:12:16 +0530

    Set up

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,24 @@
+import 'package:amazonui/screens/home_screen.dart';
+import 'package:amazonui/screens/nav_screen.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: const [
+          NavScreen(),
+          HomeScreen(),
+        ],
+      ),
+    );
+  }
+}
